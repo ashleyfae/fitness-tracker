b173924a9d4e40916ff36b88b37c46f137f7bb61
Ashley Gibson <ashley@nosegraze.com>
2025-07-05 16:20:44 +0100

    Set up factory

--- database/factories/WorkoutSessionFactory.php
+++ database/factories/WorkoutSessionFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Routine;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WorkoutSession>
@@ -16,8 +19,15 @@ class WorkoutSessionFactory extends Factory
      */
     public function definition(): array
     {
+        $startedAt = Carbon::createFromTimestamp($this->faker->dateTime('-1 day')->getTimestamp());
+
         return [
-            //
+            'user_id' => User::factory(),
+            'routine_id' => Routine::factory(),
+            'started_at' => $startedAt->toDateTimeString(),
+            'ended_at' => $startedAt->addMinutes($this->faker->numberBetween(30, 120))->toDateTimeString(),
+            'total_exercises' => $this->faker->numberBetween(1, 10),
+            'total_kg_lifted' => $this->faker->numberBetween(10, 100),
         ];
     }
 }
