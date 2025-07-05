445cc288de35609e6138fb9669d3b6515c55cbc5
Ashley Gibson <ashley@nosegraze.com>
2025-07-05 12:39:03 +0100

    Allow searching by name

--- app/Actions/Exercises/ListExercises.php
+++ app/Actions/Exercises/ListExercises.php
@@ -11,16 +11,19 @@ namespace App\Actions\Exercises;
 
 use App\Http\Requests\ListExercisesRequest;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
 
 class ListExercises
 {
     public function fromRequest(ListExercisesRequest $request): Collection|Paginator
     {
-        $request = $request->validated();
+        $data = $request->validated();
 
         $exercises = $request->user()
             ->exercises()
+            ->when(! empty($data['search']), fn(\Illuminate\Contracts\Database\Eloquent\Builder $builder) => $builder->whereLike('name', '%'.$data['search'].'%'))
             ->orderBy('name')
             ->paginate(40);
 
