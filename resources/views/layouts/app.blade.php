b500e0f72bff9fbe8fad899cc5a03986c4c4a8ae
Ashley Gibson <ashley@nosegraze.com>
2025-10-08 09:09:17 +0100

    Start styling menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,15 @@
 
 @section('base')
     <div class="container">
-        <nav id="nav">
-            <ul>
-                <li><a href="{{ route('workouts.create') }}">Start Workout</a></li>
-                <li><a href="{{ route('routines.index') }}">Manage Routines</a></li>
-                <li><a href="{{ route('exercises.index') }}">Manage Exercises</a></li>
-            </ul>
-        </nav>
+        <div id="header">
+            <nav>
+                <ul>
+                    <li><a href="{{ route('workouts.create') }}">Start Workout</a></li>
+                    <li><a href="{{ route('routines.index') }}">Manage Routines</a></li>
+                    <li><a href="{{ route('exercises.index') }}">Manage Exercises</a></li>
+                </ul>
+            </nav>
+        </div>
 
         @yield('app')
     </div>
